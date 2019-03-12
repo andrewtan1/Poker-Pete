f595e3bf8d6ad50005ed6744fc1a88515cad950b
dustinru <dalru91765@gmail.com>
2019-03-12 11:08:01 -0700

    cv2 img function demo

--- carddetection.py
+++ carddetection.py
@@ -1,7 +1,14 @@
+import numpy as np
 import cv2
-import numpy
-blahhhh = 5
-print("what is going on")
-print("check python3.7")
-print("test python3.7")
 
+# Test load img in grayscale
+img = cv2.imread('img/cards-[C0]-001.jpg',0)
+scale_percent = 20
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+# resize image
+resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+cv2.imshow('image',resized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
