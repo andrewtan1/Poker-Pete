e8e6a0ad082d964ccd787b1723aa5e95a177b568
andrewtan1 <andrewytan1@gmail.com>
2019-05-14 16:46:19 -0700

    'a'

--- poker hand.py
+++ poker hand.py
@@ -9,6 +9,48 @@
 
 import numpy as np
 import random
+def rankMap(rank):
+    rankNum = {
+        0 : "Ace",
+        1 : "Two",
+        2 : "Three",
+        3 : "Four",
+        4 : "Five",
+        5 : "Six",
+        6 : "Seven",
+        7 : "Eight",
+        8 : "Nine",
+        9 : "Ten",
+        10 : "Jack",
+        11 : "Queen",
+        12 : "King",
+        13 : "Ace",
+    }
+    return rankNum.get(rank,"invalid rank")
+# Map Suits to Numbers for Calculations
+def suitMap(suit):
+    suitNum = {
+        0 : "Diamonds",
+        1 : "Clubs",
+        2 : "Hearts",
+        3 : "Spades"
+    }
+    return suitNum.get(suit,"invalid suit")
+def bin(n, k):
+    """
+    A fast way to calculate binomial coefficients by Andrew Dalke.
+    See http://stackoverflow.com/questions/3025162/statistics-combinations-in-python
+    """
+    if 0 <= k <= n:
+        ntok = 1
+        ktok = 1
+        for t in range(1, min(k, n - k) + 1):
+            ntok *= n
+            ktok *= t
+            n -= 1
+        return ntok // ktok
+    else:
+        return 0
 
 # generate a card hand of size n (WILL NOT BE USED IN ACTUAL GAME)
 def generate_hand(n):
@@ -38,32 +80,33 @@ def read_hand(hand):
     for i in range(n):
         r = ranks[i]
         s = suits[i]
-        str1 = " "
-        str2 = " "
-        #between 1-9 means the card's rank is from 2 to 10
-        if 0 < r and r < 10:
-            str1 = str(r+1)
-        elif r == 10:
-            str1 = "Jack"
-        elif r == 11:
-            str1 = "Queen"
-        elif r == 12:
-            str1 = "King"
-        elif r == 13:
-            str1 = "Ace"
-        
-        if s == 0:
-            str2 = "Diamonds"
-        elif s == 1:
-            str2 = "Clubs"
-        elif s == 2:
-            str2 = "Hearts"
-        elif s == 3:
-            str2 = "Spades"
+        str1 = rankMap(r)
+        str2 = suitMap(s)
         print(str1, end =" ") 
         print("of", end = " ")
         print(str2)
     return
+
+# compare two cards
+
+def compare_cards(c1, c2):
+    winner = 0
+    r1 = get_ranks(c1)
+    r2 = get_ranks(c2)
+    s1 = get_suits(c1)
+    s2 = get_suits(c2)
+
+    if r1 > r2:
+        winner = 1
+    elif r1 < r2:
+        winner = 2
+    else:
+        if s1 > s2:
+            winner = 1
+        elif s1 < s2:
+            winner = 2
+
+    return winner
 
 # return the hand's highest card
 def get_highest_card(hand):
@@ -145,45 +188,116 @@ def measure_strength(hand):
     pairs = multiples[0]
     triples = multiples[1]
     quadruples = multiples[2]
+    high = get_highest_card(hand)
+    ranks = get_ranks(hand)
+    suits = get_suits(hand)
+    high_rank = get_ranks(high)[0]
 
     if straight_flush:
-        return 8
+        # A 2 3 4 5 straight flush
+        if 13 in ranks and 4 in ranks:
+            s = 4
+        else:
+            s = high_rank
+        return s+ 800
+
     if quadruples >= 1:
-        return 7
+        unique, counts = np.unique(ranks, return_counts = True)
+        for i in range(unique.size):
+            if counts[i] == 4:
+                s = unique[i]           
+        return s + 700
+
     if triples >= 1 and pairs >= 1:
-        return 6
+        unique, counts = np.unique(ranks, return_counts = True)
+        for i in range(unique.size):
+            if counts[i] == 3:
+                s = unique[i]
+        return s + 600
     if flush:
-        return 5
+        return suits[0] + 500
     if straight:
-        return 4
+        if 13 in ranks and 4 in ranks:
+            s = 4
+        else:
+            s = high_rank
+        return s + 400
     if triples >= 1:
-        return 3
+        unique, counts = np.unique(ranks, return_counts = True)
+        for i in range(unique.size):
+            if counts[i] == 3:
+                s = unique[i]
+        return s + 300
+
     if pairs >= 2:
-        return 2
+        unique, counts = np.unique(ranks, return_counts = True)
+        s = 0
+        for i in range(unique.size):
+            if counts[i] == 2 and unique[i] > s:
+                s = unique[i]
+        return s + 200
+
     if pairs == 1:
-        return 1
-    return 0
+        unique, counts = np.unique(ranks, return_counts = True)
+        for i in range(unique.size):
+            if counts[i] == 2:
+                s = unique[i]
+        return s + 100
+
+    else:
+        return high_rank
 
 def read_strength(hand):
     s = measure_strength(hand)
-    if s == 8:
-        print("Straight Flush")
-    elif s == 7:
-        print("Four of a Kind")
-    elif s == 6:
-        print("Full House")
-    elif s == 5:
-        print("Flush")
-    elif s == 4:
-        print("Straight")
-    elif s == 3:
-        print("Three of a Kind")
-    elif s == 2:
+    t = s % 100
+    rank = rankMap(t)
+    high_card = get_highest_card(hand)
+    high_rank = rankMap(get_ranks(high_card)[0])
+    high_suit = suitMap(get_suits(high_card)[0])
+    
+
+    if s == 813:
+        print("Royal Flush of ", end = " ")
+        print(high_suit)
+
+    elif s > 800:
+        print("Straight Flush with leading card")
+        print(high_rank, end = " of ")
+        print(high_suit)
+
+    elif s > 700:
+        print("Four of a Kind of", end = " ")
+        print(rank)
+
+    elif s > 600:
+        print("Full House of", end = " ")
+        print(rank)
+
+    elif s > 500:
+        print("Flush of", end = " ")
+        print(high_suit, end = ". Highest card is ")
+        print(high_rank, end = " of ")
+        print(high_suit)
+
+    elif s > 400:
+        print("Straight with leading card", end =" ")
+        print(high_rank, end = " of ")
+        print(high_suit)
+    elif s > 300:
+        print("Three of a Kind of", end = " ")
+        print(rank)
+    elif s > 200:
         print("Two Pairs")
-    elif s == 1:
-        print("One Pair")
+    elif s > 100:
+        print("One Pair of", end = " ")
+        print(rank)
     else:
-        print("Nothing")
+        print("Nothing. Highest card is", end = " ")
+        print(high_rank, end = " of ")
+        print(high_suit)
+        
+
+    
     return
 
 def recommend_move(hand):
@@ -281,23 +395,52 @@ def recommend_move(hand):
     print(" ")
     return
 
+# estimate the probability of getting a LOWER hand
+def estimate_probability(hand):
+    s = measure_strength(hand)
+    
+    if s > 800:
+        return 1 - 0.000015
+    elif s > 700:
+        return 1 - 0.000256
+    elif s > 600:
+        return 1 - 0.0017
+    elif s > 500:
+        return 1 - 0.00367
+    elif s > 400:
+        return 1 - 0.0076
+    elif s > 300:
+        return 1- 0.0287
+    elif s > 200: 
+        return 1 - 0.0762
+    elif s > 100:
+        #TODO: compute probability
+        p = 1 - 0.499
+        return p
+    else:
+        #TODO: compute probability 
+        p = 0
+        highest_rank = get_ranks(get_highest_card(hand))
+        for i in range(6,13):
+            if i == highest_rank:
+                return p
+            p = p + (4*(bin(i-1,4)*pow(4,4) - pow(4,4) - bin(i-1,4) + 1))/(bin(52,5))
+        return p
+
 
 def main():
-    #hand = np.array([0,2,4, 5,38])
+    hand = np.array([0,9,10,11,12])
+    
     hand = generate_hand(5)
-    ranks = get_ranks(hand)
-    suits = get_suits(hand)
     print(hand)
-    print(ranks)
-    print(suits)
     read_hand(hand)
-    high_card = get_highest_card(hand)
-    print("The highest card is", end = " ")
-    read_hand(high_card)
+    print("Strength is", end = " ")
+    print(measure_strength(hand))
     print("You have", end = " ")
     read_strength(hand) 
     recommend_move(hand)
-
+    #print("The probability of a lower hand is", end = " ")
+    #print(estimate_probability(hand))
     
     
        
--- zero sum.py
+++ zero sum.py
@@ -48,124 +48,15 @@ def solve(A):
 
     return  (x,y,value)
 
+
 def main():
-    """
+    
     A = [[1,-2,3],
     [-2,1,-1],
     [4,-4,0],
     [10,-10,1]]
     solve(A)
-    """
-    n = 3
-
-    p_junk = 0.501177
-    p_pair = 0.422569
-    #p_two_pair = 0.047539
-    p_high = 1 - p_junk - p_pair #- p_two_pair
-    p = [p_high, p_pair, p_junk]
-    prob = np.zeros((n,n))
-    for i in range(n):
-        for j in range(n):
-            prob[i,j] = p[i]*p[j]
     
-
-    p1 = ["" for x in range(pow(3,n))]
-    p2 = ["" for x in range(pow(4,n))]
-
-
-    for i in range(3):
-        for j in range(3):
-            for k in range(3):
-            
-                str = ""
-                if i == 0:
-                    str = str + "A"
-                elif i == 1:
-                    str = str + "S"
-                elif i == 2:
-                    str = str + "P"
-                if j == 0:
-                    str = str + "A"
-                elif j == 1:
-                    str = str + "S"
-                elif j == 2:
-                    str = str + "P"
-                if k == 0:
-                    str = str + "A"
-                elif k == 1:
-                    str = str + "S"
-                elif k == 2:
-                    str = str + "P"
-                p1[k + 3*j + 9*i] = str
-    
-    for i in range(4):
-        for j in range(4):
-            for k in range(4):
-            
-                str = ""
-                if i == 0:
-                    str = str + "A"
-                elif i == 1:
-                    str = str + "S"
-                elif i == 2:
-                    str = str + "C"
-                elif i == 3:
-                    str = str + "P"
-                if j == 0:
-                    str = str + "A"
-                elif j == 1:
-                    str = str + "S"
-                elif j == 2:
-                    str = str + "C"
-                elif j == 3:
-                    str = str + "P"
-                if k == 0:
-                    str = str + "A"
-                elif k == 1:
-                    str = str + "S"
-                elif k == 2:
-                    str = str + "C"
-                elif k == 3:
-                    str = str + "P"
-
-                
-                p2[k + 4*j + 16*i] = str
-    
-    A = np.zeros((pow(3,n), pow(4,n)))
-    for i in range(pow(3,n)):
-        for j in range(pow(4,n)):
-            str1 = p1[i]
-            str2 = p2[j]
-            sum = 0
-            for k in range(n):
-                for l in range(n):
-                    payoff = 0
-                    # k is P1's hand strength, l is P2's
-                    s1 = str1[k]
-                    s2 = str2[l]
-                    if (s1 == "A" and s2 == "A") or (s1 == "A" and s2 == "S") or (s1 == "S" and s2 == "A") or (s1 == "S" and s2 == "C"):
-                        if k < l:
-                            payoff = 2
-                        elif k == l:
-                            payoff = 0
-                        else:
-                            payoff = -2
-                    elif (s1 == "S" and s2 == "S") and (s1 == "S" and s2 == "P") or (s1 == "P" and s2 == "S") or (s1 == "P" and s2 == "P"):
-                        if k < l:
-                            payoff = 1
-                        elif k == l:
-                            payoff = 0
-                        else:
-                            payoff = -1
-                    elif (s1 == "A" and s2 == "C") or (s1 == "A" and s2 == "P"):
-                        payoff = 1
-                    elif (s1 == "P" and s2 == "A") or (s1 == "P" and s2 == "C"):
-                        payoff = -1
-
-                    sum = sum + prob[k,l]*payoff
-            
-
-            A[i,j] = sum
     (x,y,value) = solve(A)
     print("The value of the game is", end = " ")
     print(value)
@@ -173,18 +64,6 @@ def main():
     print(x)
     print("Player 2's optimal strategy is", end = " ")
     print(y)
-    for i in range(pow(4,n)):
-        if(y[i] > 0.001):
-            print(p2[i], end = " ")
-            print(y[i])
-    r = random.uniform(0,1)
-    k = 0
-    while r > 0:
-        r = r - y[k]
-        k = k + 1
-    
-        
-
-    
+   
 if __name__ == "__main__" :
     main()
